60c0cb3ef94d7a2cb1a741f96324609eec0a25d6
yatharth0 <82802443+yatharth0@users.noreply.github.com>
2025-04-11 23:18:50 +0530

    Adding project files

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,4 @@
-// const express = require('express');
-// const http = require('http');
-// const WebSocket = require('ws');
 
-// const app = express();
-// const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server });
-
-// const port = 5000;
-// let visitCount = 0;
-
-// // WebSocket connection handling
-// wss.on('connection', (ws) => {
-//   // Increment visit count when a new client connects
-//   visitCount++;
-//   // Broadcast the updated visit count to all connected clients
-//   broadcastVisitCount();
-
-//   ws.on('close', () => {
-//     // Decrement visit count when a client disconnects
-//     visitCount--;
-//     // Broadcast the updated visit count to all connected clients
-//     broadcastVisitCount();
-//   });
-// });
-
-// function broadcastVisitCount() {
-//   const message = JSON.stringify({ visits: visitCount });
-//   wss.clients.forEach((client) => {
-//     if (client.readyState === WebSocket.OPEN) {
-//       client.send(message);
-//     }
-//   });
-// }
-
-// // Route to get the current visit count
-// app.get('/visitcount', (req, res) => {
-//   res.json({ visits: visitCount });
-// });
-
-// server.listen(port, () => {
-//   console.log(`Backend Server running at http://localhost:${port}`);
-// });
 
 
 const express = require('express');
@@ -55,10 +13,8 @@ const wss = new WebSocket.Server({ server });
 const port = 5000;
 let visitCount = 0;
 
-// CORS setup
 app.use(cors());
 
-// WebSocket connection handling
 wss.on('connection', (ws) => {
   visitCount++;
   broadcastVisitCount();
@@ -68,6 +24,7 @@ wss.on('connection', (ws) => {
     broadcastVisitCount();
   });
 });
+
 
 function broadcastVisitCount() {
   const message = JSON.stringify({ visits: visitCount });
